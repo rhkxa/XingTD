2ffbd6183c8e16c068eef993208bdb168cdae004
rhkxa724. <361074968@qq.com>
2016-02-25 16:06:41 +0800

    百度离线地图修改

--- src/com/gps808/app/activity/DownloadMapActivity.java
+++ src/com/gps808/app/activity/DownloadMapActivity.java
@@ -1,21 +1,27 @@
 package com.gps808.app.activity;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
 import android.widget.RadioGroup;
-import android.widget.RadioGroup.OnCheckedChangeListener;
-
+import android.widget.TextView;
 import com.gps808.app.R;
-
-import com.gps808.app.fragment.HeaderFragment;
-import com.gps808.app.fragment.OffLineMapFragment;
-import com.gps808.app.fragment.OnLineMapFragment;
+import com.gps808.app.adapter.OfflinePagerAdapter;
 
 import com.gps808.app.utils.BaseActivity;
 
-
-public class DownloadMapActivity extends BaseActivity {
+public class DownloadMapActivity extends BaseActivity implements
+		OnPageChangeListener, OnClickListener {
 	private RadioGroup map_rg;
+	private ViewPager mContentViewPage;
+	private PagerAdapter mPageAdapter;
+	private TextView mDownloadText;
+	private TextView mDownloadedText;
+	private ImageView mBackImage;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -27,30 +33,106 @@ public class DownloadMapActivity extends BaseActivity {
 
 	private void init() {
 		// TODO Auto-generated method stub
-		HeaderFragment headerFragment = (HeaderFragment) this
-				.getSupportFragmentManager().findFragmentById(R.id.title);
-		headerFragment.setTitleText("地图下载");
-		map_rg = (RadioGroup) findViewById(R.id.map_rg);
-		map_rg.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+	
+		// view pager 用到了所有城市list和已下载城市list所有放在最后初始化
+		mContentViewPage = (ViewPager) findViewById(R.id.content_viewpage);
 
-			@Override
-			public void onCheckedChanged(RadioGroup arg0, int arg1) {
-				// TODO Auto-generated method stub
-				Fragment fragment = null;
-				switch (arg0.getCheckedRadioButtonId()) {
-				case R.id.online_map:
-					fragment = new OnLineMapFragment();
-					break;
+		mPageAdapter = new OfflinePagerAdapter(getSupportFragmentManager());
 
-				case R.id.offline_map:
-					fragment=new OffLineMapFragment();
-					break;
-				}
-				getSupportFragmentManager().beginTransaction().replace(
-						R.id.show_map, fragment).commit();
-			}
-		});
-		getSupportFragmentManager().beginTransaction().replace(
-				R.id.show_map, new OnLineMapFragment()).commit();
+		mContentViewPage.setAdapter(mPageAdapter);
+		mContentViewPage.setCurrentItem(0);
+		mContentViewPage.setOnPageChangeListener(this);
+		// 顶部
+		mDownloadText = (TextView) findViewById(R.id.download_list_text);
+		mDownloadedText = (TextView) findViewById(R.id.downloaded_list_text);
+
+		mDownloadText.setOnClickListener(this);
+		mDownloadedText.setOnClickListener(this);
+		mBackImage = (ImageView) findViewById(R.id.back_image_view);
+		mBackImage.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		if (v.equals(mDownloadText)) {
+			int paddingHorizontal = mDownloadText.getPaddingLeft();
+			int paddingVertical = mDownloadText.getPaddingTop();
+			mContentViewPage.setCurrentItem(0);
+
+			mDownloadText
+					.setBackgroundResource(R.drawable.offlinearrow_tab1_pressed);
+
+			mDownloadedText
+					.setBackgroundResource(R.drawable.offlinearrow_tab2_normal);
+
+			mDownloadedText.setPadding(paddingHorizontal, paddingVertical,
+					paddingHorizontal, paddingVertical);
+
+			mDownloadText.setPadding(paddingHorizontal, paddingVertical,
+					paddingHorizontal, paddingVertical);
+
+
+
+		} else if (v.equals(mDownloadedText)) {
+			int paddingHorizontal = mDownloadedText.getPaddingLeft();
+			int paddingVertical = mDownloadedText.getPaddingTop();
+			mContentViewPage.setCurrentItem(1);
+
+			mDownloadText
+					.setBackgroundResource(R.drawable.offlinearrow_tab1_normal);
+			mDownloadedText
+					.setBackgroundResource(R.drawable.offlinearrow_tab2_pressed);
+			mDownloadedText.setPadding(paddingHorizontal, paddingVertical,
+					paddingHorizontal, paddingVertical);
+			mDownloadText.setPadding(paddingHorizontal, paddingVertical,
+					paddingHorizontal, paddingVertical);
+
+		
+
+		} else if (v.equals(mBackImage)) {
+			// 返回
+			finish();
+		}
+
+	}
+
+	@Override
+	public void onPageScrollStateChanged(int arg0) {
+
+	}
+
+	@Override
+	public void onPageScrolled(int arg0, float arg1, int arg2) {
+
+	}
+
+	@Override
+	public void onPageSelected(int arg0) {
+		int paddingHorizontal = mDownloadedText.getPaddingLeft();
+		int paddingVertical = mDownloadedText.getPaddingTop();
+
+		switch (arg0) {
+		case 0:
+			mDownloadText
+					.setBackgroundResource(R.drawable.offlinearrow_tab1_pressed);
+			mDownloadedText
+					.setBackgroundResource(R.drawable.offlinearrow_tab2_normal);
+			// mPageAdapter.notifyDataSetChanged();
+			break;
+		case 1:
+			mDownloadText
+					.setBackgroundResource(R.drawable.offlinearrow_tab1_normal);
+
+			mDownloadedText
+					.setBackgroundResource(R.drawable.offlinearrow_tab2_pressed);
+			// mDownloadedAdapter.notifyDataChange();
+			break;
+		}
+		// handler.sendEmptyMessage(UPDATE_LIST);
+		mDownloadedText.setPadding(paddingHorizontal, paddingVertical,
+				paddingHorizontal, paddingVertical);
+		mDownloadText.setPadding(paddingHorizontal, paddingVertical,
+				paddingHorizontal, paddingVertical);
+
 	}
 }
--- src/com/gps808/app/fragment/OffLineMapFragment.java
+++ src/com/gps808/app/fragment/OffLineMapFragment.java
@@ -1,18 +1,61 @@
 package com.gps808.app.fragment;
 
+import java.util.ArrayList;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
+import com.baidu.mapapi.map.offline.MKOLUpdateElement;
+import com.baidu.mapapi.map.offline.MKOfflineMap;
+import com.baidu.mapapi.map.offline.MKOfflineMapListener;
+import com.gps808.app.R;
+import com.gps808.app.adapter.LocalMapAdapter;
 import com.gps808.app.utils.BaseFragment;
 
-public class OffLineMapFragment extends BaseFragment{
+public class OffLineMapFragment extends BaseFragment implements
+		MKOfflineMapListener {
+
+	private View view;
+	private ListView down_finish_list, down_load_list;
+	private MKOfflineMap mOffline = null;
+	/**
+	 * 已下载的离线地图信息列表
+	 */
+	private ArrayList<MKOLUpdateElement> localMapList = null;
+	private LocalMapAdapter lAdapter = null;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
-		return super.onCreateView(inflater, container, savedInstanceState);
+		view = inflater.inflate(R.layout.fragment_offline, null);
+		init();
+		return view;
+	}
+
+	private void init() {
+		// TODO Auto-generated method stub
+		mOffline = new MKOfflineMap();
+		mOffline.init(this);
+		lAdapter = new LocalMapAdapter(getActivity(), localMapList);
+		down_finish_list = (ListView) view.findViewById(R.id.down_finish_list);
+		down_load_list = (ListView) view.findViewById(R.id.down_load_list);
+
+		// 获取已下过的离线地图信息
+		localMapList = mOffline.getAllUpdateInfo();
+		if (localMapList == null) {
+			localMapList = new ArrayList<MKOLUpdateElement>();
+		}
+		lAdapter = new LocalMapAdapter(getActivity(), localMapList);
+		down_finish_list.setAdapter(lAdapter);
+	}
+
+	@Override
+	public void onGetOfflineMapState(int arg0, int arg1) {
+		// TODO Auto-generated method stub
+
 	}
 }
